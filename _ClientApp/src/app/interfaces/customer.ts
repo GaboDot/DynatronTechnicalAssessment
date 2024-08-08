15a37a01d4a00ed75aba2681d450143cc2701dab
GaboDot <gfloresolea@gmail.com>
2024-08-08 12:22:03 -0600

    Added interface: Customer.

---
+++ _ClientApp/src/app/interfaces/customer.ts
@@ -0,0 +1,10 @@
+export class Customer {
+    customerId: number = 0;
+    firstName: string;
+    lastName: string;
+    email: string;
+    status: boolean;
+    createdDate: string;
+    updatedDate: string;
+    userId: number;
+}
