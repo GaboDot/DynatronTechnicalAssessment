26c836a1e0860bfc983894367cd4b99359646783
GaboDot <gfloresolea@gmail.com>
2024-08-08 12:19:18 -0600

    Enabled: Edit button | Switch for managin active/inactive status.

--- _ClientApp/src/app/components/layout/pages/manage-users/manage-users.component.ts
+++ _ClientApp/src/app/components/layout/pages/manage-users/manage-users.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { Customer } from 'src/app/interfaces/customer';
 import { SessionService } from 'src/app/reusable/shared/session.service';
 import { UtilityService } from 'src/app/reusable/shared/utility.service';
 import { CustomerService } from 'src/app/services/customer.service';
@@ -14,8 +16,8 @@ import { CustomerService } from 'src/app/services/customer.service';
 })
 export class ManageUsersComponent {
 
-  customersData = new MatTableDataSource();
-  columnsTable: string[] = ['customerId', 'firstName', 'lastName', 'email', 'status', 'createdDate', 'updatedDate'];
+  customersData = new MatTableDataSource([]);
+  columnsTable: string[] = ['customerId', 'firstName', 'lastName', 'email', 'status', 'createdDate', 'updatedDate', 'actions'];
   @ViewChild(MatPaginator) pagTable!: MatPaginator;
   @ViewChild('customersSort') customersSort = new MatSort();
 
@@ -30,12 +32,12 @@ export class ManageUsersComponent {
   getCustomers() {
     this._customerService.getAllCustomers().subscribe({
       next: (response) => {
-        console.log(response.value);
         if(response.status) {
+          console.log(response.value);
           this.customersData.data = response.value;
           this.customersData.paginator = this.pagTable;
         }
-        else this._utilityService.showAlert('No data found.', 'Close', 'notif-warning');
+        else this._utilityService.showAlert('No data found.', '✖', 'notif-warning');
       },
       error: (e) => {}
     });
@@ -61,5 +63,35 @@ export class ManageUsersComponent {
   filterData(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.customersData.filter = filterValue.trim().toLowerCase();
+  }
+
+  udpateCustomerStatus($event: MatSlideToggleChange, toUpdate: Customer) {
+
+    let customerIdx = this.customersData.data.findIndex((obj: Customer) => {
+      return obj.customerId === toUpdate.customerId;
+    });
+
+    (this.customersData.data[customerIdx] as Customer).status = $event.checked;
+    this._utilityService.showLoadingSpinner();
+
+    this._customerService.updateCustomer(toUpdate).subscribe({
+      next: (response) => {
+        if(response.status) {
+          setTimeout(() => {
+            this._utilityService.closeLoadingSpinner()
+            this._utilityService.showAlert('Status updated!', '✖', 'notif-success')
+          }, 500);
+          
+        }
+        else {
+          (this.customersData.data[customerIdx] as Customer).status = !$event.checked;
+          setTimeout(() => {
+            this._utilityService.closeLoadingSpinner()
+            this._utilityService.showAlert('Unable to update status', '✖', 'notif-error');
+          }, 500);
+        }
+      },
+      error: (e) => { }
+    });
   }
 }
